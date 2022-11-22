f96758efcc0eb1d0aaffbcf64d99bef25d93706b
root <root@ip-172-31-44-85.ap-northeast-2.compute.internal>
2022-11-22 04:31:25 +0000

    docker-test

--- docker/node-docker-test/app.js
+++ docker/node-docker-test/app.js
@@ -1,12 +1,11 @@
 'use strict'
 
-const express = require("express"),
-const app = express();
+const express = require("express");
 const PORT = 8000;
-const HOST = '0.0.0.0'
-
+const HOST = '0.0.0.0';
+const app = express();
 const routes = require("./routes/");
-app.use(routes)
+app.use(routes);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
